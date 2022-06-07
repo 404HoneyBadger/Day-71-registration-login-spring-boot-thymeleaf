04e3c9fe51f1bf28b79f304dc0123bd721352c01
404honeybadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-06-07 19:30:52 -0400

    second commit

--- src/main/java/javaguides/springboot/config/SecurityConfiguration.java
+++ src/main/java/javaguides/springboot/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package javaguides.springboot.springsecurity.configuration;
+package javaguides.springboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
